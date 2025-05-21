b5268b86983db947ef5d1cc8a21b3777c338459a
_mhh <166766597+Mhhxl@users.noreply.github.com>
2025-05-21 16:21:42 +0000

    salvando

--- atv pontuada 4/questões/1.py
+++ atv pontuada 4/questões/1.py
@@ -1,169 +1,171 @@
 import os
-os.system('cls || clear')
 import time
 from dataclasses import dataclass
-import csv
 
-#criando lista
-lista_funcionarios= []
-
-#funções
 def limpar_terminal():
-    os.system('cls || clear')
+    # Limpar o terminal
+    os.system("cls || clear")
 
+# Criando lista de funcionário
+lista_funcionarios = []
 
-#Classe de funcionarios@dataclass
-class funcionario:
+@dataclass
+class Funcionario:
     nome: str
+    cpf: str
     cargo: str
     salario: str
+                
+    def exibir_dados(self):
+        print("Dados registrados com sucesso.")
 
-
-
-#CRUD
+# Verificando se a lista estiver vazia e fazendo o CRUD
 def verificar_lista_vazia(lista_funcionarios):
     if not lista_funcionarios:
         return True
     return False
 
-
-
-def adicionar_funcionario(lista_funcionarios):
+def cadastrando_funcionario(lista_funcionarios):
     limpar_terminal()
-    print("Carregando...")
     time.sleep(2)
-    limpar_terminal()
-    nome= input("Digite o seu nome: ")
-    cargo= input("Digite o cargo que ocupa na empresa: ")
-    salario= input("Digite o seu salário: ")
+    nome = input("Digite o nome do(a) funcionário(a): ")    
+    cpf = input(f"Digite o CPF do(a) funcionário(a) '{nome}': ")    
+    cargo = input(f"Digite o cargo atual de '{nome}': ")    
+    salario = (input(f"Digite o salário atual de '{nome}': R$ "))
 
-    limpar_terminal()
-    print("Adicionando Funcionario...")
-    Funcionarios= funcionario(nome, cargo, salario)
+    funcionario = Funcionario(nome, cpf, cargo, salario)
     lista_funcionarios.append(funcionario)
-    print(f"Funcionario '{nome}' Adicionado com sucesso! ")
-
-
-
+    print(f"O cadastro de '{nome}' foi concluído com sucesso!")
 
 def listar_funcionarios(lista_funcionarios):
-    if verificar_lista_vazia(lista_funcionarios):
-        print("A lista de funcionarios está vazia! Adicione um funcionario antes de listar.")
-        return
+    verificar_lista_vazia(lista_funcionarios)
     
-
     limpar_terminal()
-    print("Carregando...")
+    print("Carregando lista de funcionários...")
     time.sleep(2)
-    limpar_terminal(2)
-    print("\n= Lista de Funcionarios =")
-
-
+    limpar_terminal()
+    print("== Lista de Funcionários ==")
     for funcionario in lista_funcionarios:
         print(f"""
-            Nome= {funcionario.nome}
-            Cargo= {funcionario.cargo}
-            Salario= {funcionario.salario}""")
-        print("--------------------------------------------------------------------")
+            Nome: {funcionario.nome}
+            CPF: {funcionario.cpf}
+            Cargo: {funcionario.cargo}
+            Salário: R${funcionario.salario}""")
+        print("----------------------------------------------")
 
 def atualizar_funcionarios(lista_funcionarios):
-    if verificar_lista_vazia(lista_funcionarios):
-        print("A lista de funcionarios está vazia! Adicione um funcionario antes de atualizar.")
-        return
-    
+    verificar_lista_vazia(lista_funcionarios)
+
     limpar_terminal()
-    print("Carregando....")
     time.sleep(2)
-    limpar_terminal()
     listar_funcionarios(lista_funcionarios)
-    nome_antigo= input("Digite o nome do funcionario que  deseja atualizar:")
-    cargo_antigo= input("Digite o cargo do funcionario que deseja atualizar: ")
-    salario_antigo= input("Digite o salario do funcionario que deseja atualizar")
+    print("-- Atualize os dados do funcionário --\n")
+    
+    nome_antigo = input("Digite o nome do funcionário que deseja atualizar: ")
+    cpf_antigo = input(f"Digite o CPF de '{nome_antigo}': ")
+    cargo_antigo = input(f"Digite o cargo de '{nome_antigo}': ")
+    salario_antigo = (input(f"Digite o salário de '{nome_antigo}': R$ \n"))
 
-    limpar_terminal()
-    print("Atualizando Funcionarios...")
-    time.sleep(2)
     for funcionario in lista_funcionarios:
-        if funcionario.nome == nome_antigo and funcionario.cargo == cargo_antigo and funcionario.salario == salario_antigo:
-            print("Funcionario encontrado! ")
-            print("Digite os novos dados do funcionario: ")
-
-            novo_nome= input("Digite o novo nome: ")
-            novo_cargo= input("Digite o novo cargo: ")
-            novo_salario= input("Digite o novo salario: ")
-
-            funcionario.nome= novo_nome
-            funcionario.cargo= novo_cargo
-            funcionario.salario= novo_salario
-
-            print("Atualizando Dados....")
-            time.sleep(2)
-            print("Dados atualizados com sucesso! ")
+        if funcionario.nome == nome_antigo and funcionario.cpf == cpf_antigo:
+            funcionario.nome = input("Digite o novo nome do(a) funcionário(a): ") 
+            funcionario.cpf = input(f"Digite o novo CPF de '{funcionario.nome}': ") 
+            funcionario.cargo = input(f"Digite o novo cargo de '{funcionario.nome}': ") 
+            funcionario.salario = input(f"Digite o salário atual de '{funcionario.nome}': R$ ")
+            print(f"As informações do funcionário {nome_antigo} foram atualizadas!")
             return
-        else:
-            print(f"Funcionario '{nome_antigo}' não encontrado na lista.")
+    print(f"As informações de {nome_antigo} não foram encontradas na lista.")
 
+def excluir_dados_funcionario(lista_funcionarios):
+    verificar_lista_vazia(lista_funcionarios)
 
-
-def excluir_funcionario(lista_funcionarios):
-    if verificar_lista_vazia(lista_funcionarios):
-        print("A lista de funcionarios estpa vazia! Adicione um funcionario antes de excluir.")
-        return
-    
     limpar_terminal()
-    print("Carregando...\n")
-    limpar_terminal()
+    time.sleep(2)
     listar_funcionarios(lista_funcionarios)
-    nome_remover= input("Digite o nome que deseja excluir: ")
-    cargo_remover= input("Digite o cargo que dejesa excluir: ")
-    salario_remover= input("Digite o salario que deseja excluir: ")
+    print("== Insira os dados do funcionário que deseja excluir ==")
 
+    remover_nome = input("Digite o nome do funcionário que deseja remover: ")
+    remover_cpf = input(f"Digite o CPF de '{remover_nome}': ")
+    remover_cargo = input(f"Digite o cargo de '{remover_nome}': ")
+    remover_salario = (input(f"Digite o salário de '{remover_nome}': R$ "))
 
-    limpar_terminal()
-    print("Removendo Funcionario...")
     for funcionario in lista_funcionarios:
-        if funcionario.nome == nome_remover and funcionario.cargo == cargo_remover and funcionario.salario == salario_remover:
+        if (funcionario.nome == remover_nome and funcionario.cpf == remover_cpf and 
+        funcionario.cargo == remover_cargo and funcionario.salario == remover_salario):
+            print(f"Funcionário {remover_nome} encontrado!")
             lista_funcionarios.remove(funcionario)
-            print(f"As informações do funcionario '{nome_remover}' foram excluídas com sucesso! ")
+            print(f"O funcionário {remover_nome} foi removido com sucesso!")
             return
-        else:
-            print(f"Funcionario '{nome_remover}' não encontrado na lista.")
 
-def main():
+# Criando funções para salvar e carregar os dados dos funcionários na lista
+def salvar_dados(lista_funcionarios):
+    verificar_lista_vazia(lista_funcionarios)
+     # Salvando em um arquivo .csv
+    nome_arquivo = "funcionarios_dende_tech.csv"
+    with open(nome_arquivo, "a") as funcionarios_dende_tech:
+        for funcionario in lista_funcionarios:
+            funcionarios_dende_tech.write(f"""
+        == Funcionário==        
+        Nome: {funcionario.nome}
+        CPF: {funcionario.cpf}
+        Cargo: {funcionario.cargo}
+        Salário: R$ {funcionario.salario}\n""")    
+            funcionarios_dende_tech.write("--------------------------------------------------\n")
+
+def carregando_dados(lista_funcionarios):
+    verificar_lista_vazia(lista_funcionarios)
+    # Carregando um arquivo .csv
+    nome_arquivo = "funcionarios_dende_tech.csv"
+    try:
+        with open(nome_arquivo, "r") as funcionarios_dende_tech:
+            print(funcionarios_dende_tech.read())
+    except FileNotFoundError:
+        print("Arquivo não encontrado.") 
+        
+def main():    
+    # Função onde o menu será executado
     limpar_terminal()
+    print("Bem vindo a Dendê Tech! Para continuar aperte 'Enter'\n")
+    input()
     while True:
-        print("===Seja Bem Vindo ao Sistema de Funcionarios====")
-        print()
-        print("-=<Menu principal<=-")
-        menu=int(input("""
-    1- Adicionar Funcionario 
-    2- Listar Funcionarios
-    3- Atualizar Funcionarios
-    4- Remover Funcionarios
-    5- Sair
+        menu = int(input("""
+    - Menu da Dendê Tech -
     
-    Digite a opção desejada: """))
+    1 - Cadastrar Funcionários
+    2 - Listar Funcionários
+    3 - Atualizar Dados De Um Funcionário
+    4 - Excluir Dados De Um Funcionário
+    5 - Salvar Dados Dos Funcionários
+    6 - Carregar Dados Salvos Dos Funcionários
+    7 - Sair do Menu
+                                                     
+    Digite a opção que deseja acessar:  """))
 
         match menu:
             case 1:
-                adicionar_funcionario(lista_funcionarios)
+                cadastrando_funcionario(lista_funcionarios)
             case 2:
                 listar_funcionarios(lista_funcionarios)
-            case 3:
+                input()
+            case 3:         
                 atualizar_funcionarios(lista_funcionarios)
-            case 4:
-                excluir_funcionario(lista_funcionarios)
-            case 5:
-                print("Saindo do programa...")
-                time.sleep(1)
-                limpar_terminal()
-                return
+            case 4:         
+                excluir_dados_funcionario(lista_funcionarios)
+            case 5:         
+                salvar_dados(lista_funcionarios)
+            case 6:
+                carregando_dados(lista_funcionarios)
+            case 7:
+                print("Obrigado por ter acessado o menu da Dendê Tech.")
+                break
             case _:
-                print("Opção inválida! Tente novamente.")
-                time.sleep(2)
+                print("Opção errada. Digite mais uma vez (aperte 'Enter').")
+                input()
                 limpar_terminal()
+        
+        time.sleep(2)
+        limpar_terminal()
+
 if __name__ == "__main__":
     main()
-
-
-
+    
